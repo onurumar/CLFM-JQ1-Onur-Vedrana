7cec9ed54b5532649d2a035fc79ae5e7aaf051ea
V <maja5225@hotmail.com>
2020-06-09 10:38:34 +0200

    intermediate finished

--- manipulating_html_page_exercise_files/myscript.js
+++ manipulating_html_page_exercise_files/myscript.js
@@ -1,8 +1,27 @@
-$("form").submit(function(e) {
+// $("form").submit(function(e) {
 
-	e.preventDefault();
-	
+// 	e.preventDefault();
 	
 	
 
-});
+// });
+$(document).ready(function(){
+	//$("form").remove();
+
+	var newDivs = $(`
+		<div>New div 1</div>
+		<div>New div 2</div>
+		<div>New div 3</div>
+		<div>New div 4</div>
+		<div>New div 5</div>
+	`);
+	$("article").prepend(newDivs);
+
+	//$(":text[value]").text("Search for");
+	$(":text").val("Search for");
+	$("div").addClass("box");
+	$("a").attr("href", "http://www.codefactory.wien/");
+
+
+
+});	
