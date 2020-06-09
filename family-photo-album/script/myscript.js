144a87bbf3ad406220096e699fec57d034a0d1c3
V <maja5225@hotmail.com>
2020-06-09 13:20:43 +0200

    family photo album

--- family-photo-album/script/myscript.js
+++ family-photo-album/script/myscript.js
@@ -6,6 +6,7 @@ console.log(familyAndFriends);
 
 $(document).ready(function() {
     for (var i = 0; i < familyAndFriends.length; i++) {
+        
         var person = $(`
 		<div class="person">
             <img src="${familyAndFriends[i].image}" alt="Team Member">
@@ -19,7 +20,20 @@ $(document).ready(function() {
             </div>
         </div>
 	`);
-	$("div#friends-family").prepend(person);
+   
+    $("div#friends-family").append(person);
+    
+        var borderStyle = "";
+        if (familyAndFriends[i].relation == "sister") {
+            //borderStyle = "1px solid blue";
+            $(`div.person:eq(${i})`).addClass("sister")
+        } else if (familyAndFriends[i].relation == "friend") {
+            //borderStyle = "1px solid red";
+            $(`div.person:eq(${i})`).addClass("friend")
+        } else if (familyAndFriends[i].relation == "cousine") {
+            //borderStyle = "1px solid yellow"
+            $(`div.person:eq(${i})`).addClass("cousine")
+        }
 
     }
 
